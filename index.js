42e3e6dc7bd6d4d460fa0b47ee5ff15e1bf748bf
Jose Ordaz Suay <joseordazsuay@MBA02.local>
2024-02-11 00:00:04 +0100

    server + html con buscador

---
+++ index.js
@@ -0,0 +1,55 @@
+const blc = require('broken-link-checker');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/check", async (req, res) => {
+    const { url } = req.body;
+
+    const siteUrl = url;
+
+    const options = {
+        filterLevel: 1,
+        excludeExternalLinks: false,
+        excludeInternalLinks: false,
+        userAgent: 'Your User Agent',
+        acceptedSchemes: ['http', 'https'],
+        excludedKeywords: [],
+    };
+
+    let brokenLinks = [];
+
+    const siteChecker = new blc.SiteChecker(options, {
+        link: (result) => {
+
+            if (result.broken) {
+                
+                brokenLinks.push({
+                    url: result.url.resolved,
+                    status: result.brokenReason,
+                });
+            }
+        },
+        end: () => {
+
+            console.log('PROCESO FINALIZADO'); // Mostrar "OK" cuando el proceso ha finalizado
+            res.json(brokenLinks); // Enviar los enlaces rotos como JSON
+        },
+    });
+
+    siteChecker.enqueue(siteUrl);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
